9cff4ae560d34da61f2d47e0f9fa88a6d9d717cc
lucanovelli.boolean <luca.novelli.lavoro@gmail.com>
2024-05-05 13:42:43 +0200

    add: setting method input user and method log

---
+++ js/script-esercizio.js
@@ -0,0 +1,10 @@
+//CONTROLLO MAGGIORE
+"use strict";
+
+//RICHIESTA INPUT UTENTE
+let name_user = pront("Inserisci il tuo nome");
+let surname_user = pront("Inserisci il tuo cognome");
+let color_user = pront("Inserisci il tuo colore preferito");
+
+//STAMPO INPUT UTENTE
+console.log(name_user + surname_user + color_user);
