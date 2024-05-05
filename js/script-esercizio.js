934456b8df6a8897eff87046c16b9c0c834e38af
lucanovelli.boolean <luca.novelli.lavoro@gmail.com>
2024-05-05 13:48:01 +0200

    add: checked and modificated errors, changed type of variable and method prompt

--- js/script-esercizio.js
+++ js/script-esercizio.js
@@ -2,9 +2,9 @@
 "use strict";
 
 //RICHIESTA INPUT UTENTE
-let name_user = pront("Inserisci il tuo nome");
-let surname_user = pront("Inserisci il tuo cognome");
-let color_user = pront("Inserisci il tuo colore preferito");
+const name_user = prompt("Inserisci il tuo nome");
+const surname_user = prompt("Inserisci il tuo cognome");
+const color_user = prompt("Inserisci il tuo colore preferito");
 
 //STAMPO INPUT UTENTE
 console.log(name_user + surname_user + color_user);
