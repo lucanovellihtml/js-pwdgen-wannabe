c0d00bede75396ea5b3689eeee5ffd63bb7f9028
lucanovelli.boolean <luca.novelli.lavoro@gmail.com>
2024-05-06 11:08:06 +0200

    add:  the password generated is add into page html by method "innerHTML"

--- bonus/js/script-esercizio.js
+++ bonus/js/script-esercizio.js
@@ -8,8 +8,12 @@ const color_user = prompt("Inserisci il tuo colore preferito");
 const number_one = prompt("Inserisci il primo numero");
 const number_two = prompt("Inserisci il secondo numero");
 
+
 //CALCOLO NUMERO
 const number_total = parseInt(number_one) / parseInt(number_two);
+
+//VARIABILE TAG HTML
+const password_insecure = document.getElementById("password_insecure").innerHTML = name_user + surname_user + color_user + number_total;
 
 //STAMPO INPUT UTENTE
 console.log(name_user + surname_user + color_user + number_total);
