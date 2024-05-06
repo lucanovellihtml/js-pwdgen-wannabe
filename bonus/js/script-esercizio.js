a9d2ec24a47b9158f4c9499e07ddac15d3834004
lucanovelli.boolean <luca.novelli.lavoro@gmail.com>
2024-05-06 10:29:30 +0200

    add: setting prompt to two number and method for calculating the total number

---
+++ bonus/js/script-esercizio.js
@@ -0,0 +1,15 @@
+//CONTROLLO MAGGIORE
+"use strict";
+
+//RICHIESTA INPUT UTENTE
+const name_user = prompt("Inserisci il tuo nome");
+const surname_user = prompt("Inserisci il tuo cognome");
+const color_user = prompt("Inserisci il tuo colore preferito");
+const number_one = prompt("Inserisci il primo numero");
+const number_two = prompt("Inserisci il secondo numero");
+
+//CALCOLO NUMERO
+const number_total = parseInt(number_one) / parseInt(number_two);
+
+//STAMPO INPUT UTENTE
+console.log(name_user + surname_user + color_user + number_total);
